5159012d57d8f446fb9e47e1a303a25d6aa7ce14
matata samuel <mattasamuel3@gmail.com>
2020-10-26 12:39:05 +0300

    added project files

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa/Desktop/moringa-prj/core/Django/review/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
